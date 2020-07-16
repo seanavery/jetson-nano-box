67d3a641cc92713b7b94a149a6ca70e2dbc48204
sean <seanpollock3344@gmail.com>
2020-07-16 16:07:26 -0500

    camera tuning scripts

---
+++ scripts/fetch_wideangle_tuning_profile.sh
@@ -0,0 +1,2 @@
+wget https://www.waveshare.com/w/upload/e/eb/Camera_overrides.tar.gz
+tar zxvf Camera_overrides.tar.gz 
---
+++ scripts/replace_tuning_profile.sh
@@ -0,0 +1,3 @@
+sudo cp camera_overrides.isp /var/nvidia/nvcam/settings/
+sudo chmod 664 /var/nvidia/nvcam/settings/camera_overrides.isp
+sudo chown root:root /var/nvidia/nvcam/settings/camera_overrides.isp
